f1c7fcafa96700be5d8f4ac0985020a871130fc9
tommivk <tommikokk@gmail.com>
2022-12-30 23:20:58 +0200

    add test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,4 +4,13 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Pokemons page can be opened and correct data is displayed', function() {
+    cy.visit('http://localhost:5000')
+    const el = cy.contains('charmander')
+    el.click()
+    cy.contains('charmander')
+    cy.contains('blaze')
+    cy.contains('solar power')
+  })
 })
